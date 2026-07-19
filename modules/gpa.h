362a6172ed8a2ec5596d00308697f82e6eda2e5e
Zaafira Raisa <zfraisa2020@gmail.com>
2026-07-19 22:34:00 +0600

    Declared expected CGPA calculator

--- modules/gpa.h
+++ modules/gpa.h
@@ -10,7 +10,7 @@ extern const char *gradeLetters[];
 double getGradePoint(CourseResult result);
 char *getLetterGrade(CourseResult result);
 double calculateGPA(CourseResult results[], int n_results);
-double calculateRequiredGPA(double current_cgpa, double completed_credits,
-                            double target_cgpa, double remaining_credits);
+double calculateExpectedCGPA(double current_cgpa, double completed_credits,
+                             double expected_gpa, double future_credits);
 
 #endif
