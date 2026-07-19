17576c725bd60f21362e7893cf0db35a858af673
Zaafira Raisa <zfraisa2020@gmail.com>
2026-07-19 22:50:30 +0600

    Declared student module

---
+++ modules/student.h
@@ -0,0 +1,19 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "courseResult.h"
+
+typedef struct Student
+{
+    char id[20];
+    char name[80];
+    CourseResult courseResults[100];
+    int n_results;
+    double cgpa;
+} Student;
+
+Student createStudent(char id[], char name[]);
+void addCourseResultToStudent(Student *student, CourseResult result);
+void viewStudent(Student student);
+
+#endif
