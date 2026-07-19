81e0fc1fc3cd2979cd3c8396b361c202e7cdbb0f
Zaafira Raisa <zfraisa2020@gmail.com>
2026-07-19 21:22:56 +0600

    Declared semester course helpers

--- modules/courseResult.h
+++ modules/courseResult.h
@@ -7,11 +7,12 @@ typedef struct CourseResult
 {
     Course *course;
     double marks;
-    int completed;
 } CourseResult;
 
-CourseResult createCompletedCourseResult(Course *course, double marks);
-CourseResult createIncompleteCourseResult(Course *course);
+CourseResult createCourseResult(Course *course, double marks);
+void sortCourseResultsBySemester(CourseResult results[], int n_results);
+void filterCourseResultsBySemester(CourseResult results[], int n_results, int semester, CourseResult filtered[]);
+int countCourseResultsBeforeNull(CourseResult results[], int n_results);
 void viewCourseResult(CourseResult result);
 
 #endif
